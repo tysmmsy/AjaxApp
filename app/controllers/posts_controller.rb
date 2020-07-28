28b603a339a6ce50aca2f8ab52d0d8868c7630be
tysmmsy <68370113+tysmmsy@users.noreply.github.com>
2020-07-28 15:32:28 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,21 @@
 class PostsController < ApplicationController
-  def index #indexアクションを定義した
-    @posts = Post.all # ｓべてのレコードを@postに代入
-  end
-
-  def new
+  def index
+    @posts = Post.all.order(id: "DESC")
   end
 
   def create
     Post.create(content: params[:content])
+  end
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked then
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
   end
 end
